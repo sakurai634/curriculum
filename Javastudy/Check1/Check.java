1d77e875bba80ac2fe367c323216f42f688c5231
sakurai634 <65490681+sakurai634@users.noreply.github.com>
2020-07-26 21:35:48 +0900

    チェックテスト１
    チェックテスト１

--- Javastudy/Check1/Check.java
+++ Javastudy/Check1/Check.java
@@ -3,34 +3,32 @@
  *
  */
 public class Check {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /*
-        * 問1
-        * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
-        */
-        int data = {3,1,2,7,5};
+         * 問1 int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+         */
+        int[] data = { 3, 1, 2, 7, 5 };
         /*
-        * 問2
-        * 以下のfor文を完成させなさい
-        */
-        for(int i = 0; i < 4; i++){
-            System.out.print(data[i] + " ");
+         * 問2 以下のfor文を完成させなさい
+         */
+        for (int i = 0; i < 5; i++) {
+        System.out.print(data[i] + " ");
         }
         System.out.println();
         for (int i = 0; i < 4; i++) {
-            for (int j = /*ここに記述*/; j > i; j--) {
+            for (int j = 0; j > 4 - i; j++) {
                 /*
-                * 問3
-                * 以下、配列の添字を入れてソートを完成させなさい
-                */
-                if(data[/*ここに記述*/] > data[/*ここに記述*/]){
-                  int box = data[/*ここに記述*/];
-                  data[/*ここに記述*/] = data[/*ここに記述*/];
-                  data[/*ここに記述*/] = box;
+                 * 問3 以下、配列の添字を入れてソートを完成させなさい
+                 */
+
+                if (data[j] > data[j + 1]) {
+                    int box = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = box;
                 }
             }
         }
-        for(int i = 0; i < data.length; i++){
+        for (int i = 0; i < data.length; i++) {
             System.out.print(data[i] + " ");
         }
     }
